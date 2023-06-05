cc5b9b0c72740ee04985d8217f0bc833a11fc431
Michael Nissenbaum <mjn@Michaels-Mac-Studio.local>
2023-06-05 12:36:17 -0400

    fmt

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
+use crate::routes::{health_check, subscriptions};
 use actix_web::dev::Server;
-use actix_web::{HttpServer, web};
-use crate::routes::{subscriptions, health_check};
+use actix_web::{web, HttpServer};
+use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
@@ -9,7 +9,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscriptions::subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,11 @@
 //! tests/health_check.rs
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
 use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
@@ -27,7 +26,6 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -66,10 +64,10 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
 
-    for(invalid_body, error_message) in test_cases {
+    for (invalid_body, error_message) in test_cases {
         let response = client
             .post(&format!("{}/subscriptions", &app_address))
             .header("Content-Type", "applications/x-www-form-urlencoded")
